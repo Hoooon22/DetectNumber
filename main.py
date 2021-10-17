f532978e52b7aedf551b68cad9b3b5c5b1253d25
Hoooon22 <momo990305@gmail.com>
2021-10-17 16:27:31 +0900

    Adaptive Thresholding

--- main.py
+++ main.py
@@ -15,8 +15,30 @@ while True:
     # GrayScale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # Maximize Contrast
+    structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+    imgTopHat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, structuringElement)
+    imgBlackHat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, structuringElement)
+
+    imgGrayscalePlusTopHat = cv2.add(gray, imgTopHat)
+    gray = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+
+    # Adaptive Thresholding
+    img_blurred = cv2.GaussianBlur(gray, ksize=(5, 5), sigmaX=0)
+
+    img_thresh = cv2.adaptiveThreshold(
+        img_blurred,
+        maxValue=255.0,
+        adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        thresholdType=cv2.THRESH_BINARY_INV,
+        blockSize=19,
+        C=9
+    )
+
+
     # show Video
-    cv2.imshow("VideoFrame", gray)
+    cv2.imshow("VideoFrame", img_thresh)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
